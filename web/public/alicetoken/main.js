4c3b9bc206598b94449cdadab056631f1e5d1987
Subraiz Ahmed <subraizahmed@gmail.com>
2018-06-22 18:28:13 -0400

    Web3

--- web/public/alicetoken/main.js
+++ web/public/alicetoken/main.js
@@ -10,7 +10,7 @@ if (typeof web3 !== 'undefined') {
   // set the provider you want from Web3.providers
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
-
+console.log(web3.eth);
 // function init() {
 //
 //     // Is there is an injected web3 instance?
