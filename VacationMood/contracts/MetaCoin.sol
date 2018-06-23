e8ef8b05644dda222fc572d3da191c35799d4a1c
ha321 <40413775+ha321@users.noreply.github.com>
2018-06-22 20:53:54 -0400

    Contract

--- VacationMood/contracts/MetaCoin.sol
+++ VacationMood/contracts/MetaCoin.sol
@@ -1,34 +1,31 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
 
-import "./ConvertLib.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-// This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+/**
+ * @title CryptoPuff
+ * CryptoPuff - a contract for my non-fungible crypto puffs.
+ */
+contract VacationMood is ERC721Token, Ownable {
+    function VacationMood() ERC721Token("Alicein", "ALICE") public { }
 
-contract MetaCoin {
-	mapping (address => uint) balances;
+    /**
+    * @dev Mints a token to an address with a tokenURI.
+    * @param _to address of the future owner of the token
+    * @param _tokenURI token URI for the token
+    */
+    function mintTo(address _to, string _tokenURI) public {
+        uint256 newTokenId = _getNextTokenId();
+        _mint(_to, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+    }
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-	constructor() public {
-		balances[tx.origin] = 10000;
-	}
-
-	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		emit Transfer(msg.sender, receiver, amount);
-		return true;
-	}
-
-	function getBalanceInEth(address addr) public view returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
-	}
-
-	function getBalance(address addr) public view returns(uint) {
-		return balances[addr];
-	}
+    /**
+    * @dev calculates the next token ID based on totalSupply
+    * @return uint256 for the next token ID
+    */
+    function _getNextTokenId() private view returns (uint256) {
+        return totalSupply().add(1);
+    }
 }
